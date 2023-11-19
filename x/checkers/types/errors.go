b424304b8042be28d4369dea358b9fba2c2c0fdd
harun961018 <lievander24@gmail.com>
2023-11-19 21:50:05 +0300

    feat:Add functionality to store winner on blockchain

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,9 +8,9 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
-	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
-	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+	ErrInvalidBlack     	= sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed       	= sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable 	= sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
 	ErrInvalidGameIndex     = sdkerrors.Register(ModuleName, 1103, "game index is invalid")
 	ErrInvalidPositionIndex = sdkerrors.Register(ModuleName, 1104, "position index is invalid")
 	ErrMoveAbsent           = sdkerrors.Register(ModuleName, 1105, "there is no move")
@@ -18,4 +18,5 @@ var (
 	ErrCreatorNotPlayer     = sdkerrors.Register(ModuleName, 1107, "message creator is not a player")
 	ErrNotPlayerTurn        = sdkerrors.Register(ModuleName, 1108, "player tried to play out of turn")
 	ErrWrongMove            = sdkerrors.Register(ModuleName, 1109, "wrong move")
+	ErrGameFinished 		= sdkerrors.Register(ModuleName, 1110, "game is already finished")
 )
